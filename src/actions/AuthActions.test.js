435f6ec1416edb51d7b3795de6664bcb0e827d55
Julia Espinoza <juespinozacorrea@gmail.com>
2024-07-10 20:37:14 -0400

    Error

--- src/actions/AuthActions.test.js
+++ src/actions/AuthActions.test.js
@@ -2,7 +2,7 @@ import { logoutUser } from "./AuthActions";
 
 describe('AuthActions', () => {
     test('logout exitoso', () => {
-        expect(logoutUser()).toEqual({
+        expect(logoutUser()).toBe({
             type: 'SET_CURRENT_USER',
             payload: {
                 email: ''
